1b6b4e1c57cf11d1c43a3a1a69a8247d4e43748d
Chae-won Nam <rabbit7653@naver.com>
2023-07-11 17:13:50 +0900

    Update lambda_function.py

--- usage-EC2-report/lambda_function.py
+++ usage-EC2-report/lambda_function.py
@@ -1,17 +1,19 @@
 import boto3
-import json
+import urllib.request, urllib.parse, json
+from datetime import datetime, timezone, timedelta
 import os
 SLACK_URL = os.environ['SLACK_URL']
 
 def get_running_instances():
     ec2_client = boto3.client('ec2')
     regions = [ region['RegionName'] for region in ec2_client.describe_regions()['Regions'] ]
-    instances = []
+    running_instances = []
+    stopped_instances = []
     
     for region in regions:
         ec2_client = boto3.client('ec2', region_name=region)
-        running_ec2 = ec2_client.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-        for reservation in running_ec2['Reservations']:
+        ec2_instances = ec2_client.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'stopped']}])
+        for reservation in ec2_instances['Reservations']:
             for instance in reservation['Instances']:
                 name_value = 'None'
                 tags = instance.get('Tags', [])
@@ -19,40 +21,54 @@ def get_running_instances():
                     if tag['Key'] == 'Name':
                         name_value = tag['Value']
                         break
-                instances.append(region + " / " + instance['InstanceId'] + " / " + instance['InstanceType'] + " / " + name_value)
+                instance_state = instance['State']['Name']
+                if instance_state == 'running':
+                    seoul_timezone = timezone(timedelta(hours=9))
+                    launch_time = instance['LaunchTime'].replace(tzinfo=timezone.utc)
+                    current_time = datetime.now(timezone.utc)
+                    running_time = current_time - launch_time
+                    days = running_time.days
+                    hours = running_time.seconds // 3600
+                    minutes = (running_time.seconds % 3600) // 60
+                    instance_time = f"(Launch: {(launch_time+timedelta(hours=9)).strftime('%Y-%m-%d %H:%M')})" + f" ~ {days}일 {hours}시간 {minutes}분간 실행 중"
+                    running_instances.append(region + " / " + name_value + " / " + instance['InstanceType'] + " / " + instance['InstanceId'] + " / " + instance_state + " / " + instance_time)
+                else:
+                    stopped_instances.append(region + " / " + name_value + " / " + instance['InstanceType'] + " / " + instance['InstanceId'] + " / " + instance_state)
     
-    return instances
+    return running_instances, stopped_instances
     
 
-# def generate_mm_message(result):
-#     daily_total = 0.0
-#     temp_result = {}
-#     for r in result:
-#         temp_result[r["Keys"][0]] = float(r["Metrics"]["UnblendedCost"]["Amount"])
-#     sorted_result = dict(sorted(temp_result.items(), key=operator.itemgetter(1),reverse=True))
+def generate_mm_message(running, stopped):
+    message = "Account: bigdata@kookmin.ac.kr\n"
+    current_time = datetime.now(timezone(timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')
+    message += (current_time+"\n")
+    
+    message += "\n[Running EC2 Instances]\n"
+    for r in running:
+        message += (r+" :large_green_circle:"+"\n")
+    
+    message += "\n[Stopped EC2 Instances]\n"
+    for s in stopped:
+        message += (s+" :white_circle:"+"\n")
 
-#     message = "Acount: bigdata@kookmin.ac.kr\n"
-#     for k in sorted_result.keys():
-#         message += (k + " = " + str(sorted_result[k]) + "\n")
+    return message
 
-#     return message
+def generate_curl_message(message):
+    payload = {"text": message}
+    return json.dumps(payload).encode("utf-8")
 
-# def generate_curl_message(message):
-#     payload = {"text": message}
-#     return json.dumps(payload).encode("utf-8")
-
-# def post_message(url, data):
-#     req = urllib.request.Request(url)
-#     req.add_header("Content-Type", "application/json")
-#     return urllib.request.urlopen(req, data)
+def post_message(url, data):
+    req = urllib.request.Request(url)
+    req.add_header("Content-Type", "application/json")
+    return urllib.request.urlopen(req, data)
 
 def lambda_handler(event, context):
     url = SLACK_URL
     
-    running_instances = get_running_instances()
-    # message = generate_mm_message(running_instances)
-    # data = generate_curl_message(message)
-    # response = post_message(url, data)
+    running_instances, stopped_instances = get_running_instances()
+    message = generate_mm_message(running_instances, stopped_instances)
+    data = generate_curl_message(message)
+    response = post_message(url, data)
 
-    # return response.status
-    return running_instances
+    return response.status
+    # return message
