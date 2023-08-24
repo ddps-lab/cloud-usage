b230c1ea770ba3965d8952c80e3c7f4a914ed4dd
Chaewon14 <rabbit7653@gmail.com>
2023-08-25 00:17:12 +0900

    Feat: failed copy object list

--- monitor/aws_move_to_glacier_s3_bucket.py
+++ monitor/aws_move_to_glacier_s3_bucket.py
@@ -48,6 +48,7 @@ def go_to_glacier(go_to_glacier_bucket_list):
     all_bucket_list = s3_client.list_buckets()['Buckets']
     result_list = []
     bucket_name_max = 0
+    failed_copy_list = []
 
     for bucket in all_bucket_list:
         bucket_name = bucket["Name"]
@@ -61,28 +62,34 @@ def go_to_glacier(go_to_glacier_bucket_list):
                     object_key = obj['Key']
                     object_size = obj['Size']
                     object_storage_class = obj.get('StorageClass', 'STANDARD')
+                    if object_storage_class != 'GLACIER':
+                        try:
+                            s3_client.copy_object(Bucket=bucket_name, CopySource={'Bucket': bucket_name, 'Key': object_key}, Key=object_key, StorageClass='GLACIER')
+                        except Exception as e:
+                            failed_object = [bucket_name, object_key]
+                            failed_copy_list.append(failed_object)  # 복사 실패한 파일을 리스트에 추가
+                            continue
                     if object_size is not None:
                         bucket_size += object_size
-                    if object_storage_class != 'GLACIER':
-                        s3_client.copy_object(Bucket=bucket_name, CopySource={'Bucket': bucket_name, 'Key': object_key}, Key=object_key, StorageClass='GLACIER')
 
                 bucket_size = round(bucket_size/(1000*1000), 2)  # MB 단위
                 result = [bucket_name, bucket_size]
                 result_list.append(result)
     ordered_result_list = sorted(result_list, key=lambda x: x[1], reverse=True)
-    return ordered_result_list, bucket_name_max
+    return ordered_result_list, bucket_name_max, failed_copy_list
 
-def generate_message(go_to_glacier_bucket_list, bucket_name_max):
+def generate_message(go_to_glacier_bucket_list, bucket_name_max, failed_copy_list):
     messages = []
-    len_list = len(go_to_glacier_bucket_list)
-    header = "List of S3 Buckets successfully moved to Glacier - [" + str(len_list) + " buckets]\n"
+    len_bucket_list = len(go_to_glacier_bucket_list)
+    len_failed_list = len(failed_copy_list)
+    header = "List of S3 Buckets successfully moved to Glacier - [" + str(len_bucket_list) + " buckets]\n"
     crrent_time = datetime.now(timezone(timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')
     header += (crrent_time+"\n")
     
     if bucket_name_max < 11:
         bucket_name_max = 11
    
-    if len_list == 0:
+    if len_bucket_list == 0:
         message = "\nThere are NO Buckets moved to Glacier.\n"
     else:
         message = f'{"No":>2}. {"Bucket Name":{bucket_name_max+2}} {"Size":12}'
@@ -97,6 +104,11 @@ def generate_message(go_to_glacier_bucket_list, bucket_name_max):
             if len(message) > 3850:
                 messages.append(message)
                 message = ""
+        if len_failed_list > 0:
+            failed_message = "\n\n※ 아래는 오류로 인해 직접 glacier로 이동해야 하는 객체들입니다."
+            for item in failed_copy_list:
+                failed_message += f"\n  [{item[0]}] - {item[1]}"
+            message += failed_message
     messages.append(message)
     return header, messages
     
@@ -119,8 +131,8 @@ def lambda_handler(event, context):
     url = SLACK_URL
 
     go_to_glacier_bucket_list = get_final_archive_list_from_s3()
-    result_list, bucket_name_max = go_to_glacier(go_to_glacier_bucket_list)
-    header, messages = generate_message(result_list, bucket_name_max)
+    result_list, bucket_name_max, failed_copy_list = go_to_glacier(go_to_glacier_bucket_list)
+    header, messages = generate_message(result_list, bucket_name_max, failed_copy_list)
     
     data = generate_curl_message(header, True)
     response = post_message(url, data)
