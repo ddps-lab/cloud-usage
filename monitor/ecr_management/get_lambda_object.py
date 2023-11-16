62df51ade88f455dacad9a944b7979fceb7dc9ed
seungwoo1124 <seungwoo1124@naver.com>
2023-11-16 18:48:00 +0900

    continuing ecr/lambda information extracting

---
+++ monitor/ecr_management/get_lambda_object.py
@@ -0,0 +1,84 @@
+import boto3
+from datetime import datetime, timezone, timedelta
+
+error_list = []
+
+# region_lambda_object
+#   function_objects : list
+
+# function_object
+#   'FunctionName': string
+#   'MemorySize': int
+#   'LastModified': string
+#   'PackageType': string
+#   'ImageUri': string
+#   'Description': string
+
+def get_region_lambda_object(client, region):
+    ret = []
+    functions = []
+    marker = None
+    try:
+        while True:
+            if marker:
+                response = client.list_functions(Marker=marker)
+            else:
+                response = client.list_functions()
+            functions.extend(response['Functions'])
+            marker = response.get('NextMarker')
+            if not marker:
+                break
+    except Exception as e:
+        error_list.append((region, str(e)))
+        return ret
+
+    for function_object in functions:
+        func = {
+            'FunctionName': function_object['FunctionName'],
+            'MemorySize': function_object.get('MemorySize'), 
+            'LastModified': function_object['LastModified'], 
+            'PackageType': function_object['PackageType'],
+            'ImageUri': None,
+            'Description': function_object.get('Description')
+        }
+        if function_object['PackageType'] == 'Image':
+            try:
+                response_func = client.get_function(FunctionName=function_object['FunctionName'])
+            except Exception as e:
+                error_list.append((function_object['FunctionName'], str(e)))
+                print(e)
+                ret.append(func)
+                continue
+            func['ImageUri'] = response_func['Code']['ImageUri']
+        ret.append(func)
+    return ret
+
+
+def get_region_lambda_object_dic(session):
+    regions = session.get_available_regions('lambda')
+    ret = {}
+    for region in regions:
+        print(f"get {region} region's lambda_function_objects")
+        client = session.client('lambda', region_name=region)
+        region_object = get_region_lambda_object(client, region)
+        if len(region_object) <= 0:
+            continue
+        ret[region] = region_object
+    return ret
+
+
+if __name__ == "__main__":
+    session = boto3.Session()
+    regions = session.get_available_regions('lambda')
+    region_lambda_object_dic = get_region_lambda_object_dic(session)
+    
+    for region in region_lambda_object_dic.keys():
+        print(f"====== REGION {region} ======")
+        functions = region_lambda_object_dic[region]
+        for function_object in functions:
+            print(f"function name : {function_object['FunctionName']} / "\
+                    f"PackageType : {function_object['PackageType']} / "\
+                    f"ImageUri : {function_object['ImageUri']}")
+
+    for error in error_list:
+        print(error)
